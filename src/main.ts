bb2c67cca422483089b7b7a6523e02c9efdb8fe1
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-02-20 15:58:56 -0300

    usando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('TODOApp API')
+    .setDescription('TODOApp API Documentation')
+    .setVersion('0.0.1')
+    .addTag('todoapp')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
